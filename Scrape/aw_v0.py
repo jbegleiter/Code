98c9e5f71c5c92d7aa4239161752c0e2cd7fb52e
Jason B <jbegleiter@pinpoynthealth.com>
2011-11-02 00:45:41 -0400

    Allergy v0

--- Scrape/aw_v0.py
+++ Scrape/aw_v0.py
@@ -3,27 +3,32 @@
 import os, re, sys, urllib
 from geocode import *
 
-def route(param):
-	if param[0] == 'aw_allergies': 
-		for zip in param[2:]: #param[1] is date
-			aw_allergy(param[1],zip)
+def route(label, d_date, z_code):
+	if label == 'aw_allergies': 
+		aw_allergy(label, z_code)
 
-	elif param[0] == 'aw_respitory': 
-		for zip in param[2:]:
-			aw_respitory(param[1],zip)
+	elif label == 'aw_respitory': 
+		aw_respitory(label,z_code)
+	else: 
+		print 'wrong'
+		sys.exit(1)
 
 
 def aw_allergy(a_date, zip):
 	geo_info = geo(zip)
+	print geo_info
 	#url = urllib.open('http://www.accuweather.com/us/pa/philadelphia/19019/health-allergies.asp')
 
 def aw_respitory(r_date, zip):
+	1+1
 
 
 def main():
-	params = sys.argv[1:] #[label day zip1 zip2]
-	for param in params:
-		route(param)
+	label = sys.argv[1]
+	d_date = sys.argv[2]
+	zipcodes = sys.argv[3:] #[label day zip1 zip2]
+	for z_code in zipcodes:
+		route(label,d_date,z_code)
 
 if __name__ == '__main__':
   main()
--- Scrape/geocode.py
+++ Scrape/geocode.py
@@ -2,20 +2,22 @@
 
 import os, re, sys, urllib
 
-def valid_HTML(urlfile):
-	info = urlfile.info()
-	if info.gettype() == 'text/html': 
-		urltext = urlfile.read()
-		return urltext
-	else: 
-		print 'Error retrieving geocode - HTML improperly formatted'
-		sys.exit(1)
+
 
 def geo(zip_geo):
 	geo_data = {}
 	urlfile = urllib.urlopen('http://maps.googleapis.com/maps/api/geocode/json?address='+ zip_geo + '&sensor=true')
-	urltext = valid_HTML(urlfile)
+	urltext = urlfile.read()
 
-	sub = re.findall(r'formatted_address" : "([\w\s\.]+), (\w\w) (\d\d\d\d\d)')
+	sub = re.findall(r'formatted_address" : "([\w\s\.]+), (\w\w) (\d\d\d\d\d)',urltext)
 	print sub
  
+
+# def valid_HTML(urlfile):
+# 	info = urlfile.info()
+# 	if info.gettype() == 'text/html': 
+# 		urltext = urlfile.read()
+# 		return urltext
+# 	else: 
+# 		print 'Error retrieving geocode - HTML improperly formatted'
+# 		sys.exit(1)
